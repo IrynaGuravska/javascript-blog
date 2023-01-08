c4c627ad1579842af26c06f55b3c92c0e3398de9
Iryna Guravska <irinaguru.ua@gmail.com>
2023-01-08 14:48:34 +0200

    Add kod to get "href" and "active" to the article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+   event.preventDefault();
    const clickedElement = this;
    console.log('Link was clicked!');
    console.log(event);
@@ -12,28 +13,34 @@ const activeLinks = document.querySelectorAll('.titles a.active');
 for(let activeLink of activeLinks){
    activeLink.classList.remove('active');
 }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
    clickedElement.classList.add('active')
    console.log('clickedElement:', clickedElement);
 
 /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.post');
+const activeArticles = document.querySelectorAll('article');
 
 for(let activeArticle of activeArticles){
    activeArticle.classList.remove('active');
 }
-  /* get 'href' attribute from the clicked link */
+  /*DONE get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+const articleSelector = clickedElement.getAttribute("href");
+   console.log("Was clicked:", articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* DONE find the correct article using the selector (value of 'href' attribute) */
+
+const targetArticle = document.querySelector(articleSelector);
+   console.log("It is click", targetArticle)
+
+  /*DONE add class 'active' to the correct article */
+   
+   targetArticle.classList.add('active')  
 }
-
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-  
+   link.addEventListener('click', titleClickHandler);
 }
